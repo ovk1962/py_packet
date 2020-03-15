795f82f6d566b8c9e415a47fb5d32be6338c7338
OVK <mobile.ovk@gmail.com>
2020-03-15 21:47:15 +0400

    Signed-off-by: OVK <mobile.ovk@gmail.com>

--- py_FUT.py
+++ py_FUT.py
@@ -5,15 +5,130 @@
 #
 #  Copyright 2020 OVK <ovk.rus@gmail.com>
 #
+#=======================================================================
+import os, sys, math, time, sqlite3, logging
+from datetime import datetime, timezone
+import math
+from ipdb import set_trace as bp    # to set breakpoints just -> bp()
 import PySimpleGUI as sg
-"""
-    Demonstration of MENUS!
-    How do menus work?  Like buttons is how.
-    Check out the variable menu_def for a hint on how to
-    define menus
-"""
+#=======================================================================
+class Class_LOGGER():
+    def __init__(self, path_log):
+        #self.logger = logging.getLogger(__name__)
+        self.logger = logging.getLogger('__main__')
+        self.logger.setLevel(logging.INFO)
+        # create a file handler
+        self.handler = logging.FileHandler(path_log)
+        self.handler.setLevel(logging.INFO)
+        # create a logging format
+        #self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        self.handler.setFormatter(self.formatter)
 
+        # add the handlers to the logger
+        self.logger.addHandler(self.handler)
+    #-------------------------------------------------------------------
+    def wr_log_info(self, msg):
+        self.logger.info(msg)
+    #-------------------------------------------------------------------
+    def wr_log_error(self, msg):
+        self.logger.error(msg)
+#=======================================================================
+class Class_str_FUT():
+    fAsk, fBid = range(2)
+    def __init__(self):
+        self.ind_s, self.dt, self.arr  = 0, '', []
+    def __retr__(self):
+        return 'ind_s = {}, dt = {}{} arr={}'.format(self.ind_s, self.dt, '\n', str(self.arr))
+    def __str__(self):
+        return 'ind_s = {}, dt = {}{} arr={}'.format(self.ind_s, self.dt, '\n', str(self.arr))
+#=======================================================================
+class Class_ACCOUNT():
+    bal, prf, go, dep = range(4)
+    def __init__(self):
+        self.ss = '        bal,      prf,      go,       dep'
+        self.dt, self.arr  = '', []
+    def __retr__(self):
+        return 'dt = {}\n{}\narr={}\n'.format(self.dt, self.ss, str(self.arr))
+    def __str__(self):
+        return 'dt = {}\n{}\narr={}\n'.format(self.dt, self.ss, str(self.arr))
+#=======================================================================
+class Class_FUT():
+    sP_code, sRest, sVar_mrg, sOpen_prc, sLast_prc, sAsk, sBuy_qty, sBid, sSell_qty, sFut_go, sOpen_pos  = range(11)
+    def __init__(self):
+        self.sP_code, self.arr = '', []
+    def __retr__(self):
+        return  '{} {}'.format(self.sP_code,  str([int(k) for k in self.arr]))
+    def __str__(self):
+        return  '{} {}'.format(self.sP_code,  str([int(k) for k in self.arr]))
+#=======================================================================
+class Class_DB():
+    def __init__(self, path_db):
+        self.path_db = path_db
+        self.table_db = []
+        self.conn = ''
+        self.cur  = ''
+        # cfg_soft
+        self.titul          = ''    # term ALFA
+        self.path_file_DATA = ''    # c:\\str_log_ad_A7.txt
+        self.path_file_HIST = ''    # c:\\hist_log_ad_A7.txt
+        self.dt_start_sec   = 0     # 2017-01-01 00:00:00
+        self.path_file_TXT  = ''    # c:\\hist_log_ALOR.txt
 
+    def prn(self,
+            p_cfg_SOFT  = False,
+            ):
+        s = ''
+        try:
+            if p_cfg_SOFT:
+                frm = '{: <18}{: <55}'
+                print(frm.format('titul',          self.titul))
+                print(frm.format('dt_start',       self.dt_start))
+                print(frm.format('dt_start_sec',   str(self.dt_start_sec)))
+                print(frm.format('path_file_DATA', self.path_file_DATA))
+                print(frm.format('path_file_HIST', self.path_file_HIST))
+                print(frm.format('path_db_today',  self.path_db))
+        except Exception as ex:
+            r_prn = [1, 'op_archiv / ' + str(ex)]
+
+        return [0, s]
+
+    def op(self,
+            rd_cfg_SOFT  = False,
+
+            rd_term_FUT  = False,
+            rd_term_HST  = False,
+
+            ):
+        r_op_today = []
+        self.conn = sqlite3.connect(self.path_db)
+        try:
+            with self.conn:
+                r_op_today = [0, 'ok']
+                self.cur = self.conn.cursor()
+
+                if rd_cfg_SOFT:
+                    cfg = []
+                    self.cur.execute('SELECT * from ' + 'cfg_SOFT')
+                    cfg = self.cur.fetchall()    # read table name_tbl
+                    #
+                    for item in cfg:
+                        if item[0] == 'titul'         : self.titul           = item[1]
+                        if item[0] == 'path_file_DATA': self.path_file_DATA  = item[1]
+                        if item[0] == 'path_file_HIST': self.path_file_HIST  = item[1]
+                        if item[0] == 'dt_start'      : self.dt_start        = item[1]
+                        if item[0] == 'path_file_TXT' : self.path_file_TXT   = item[1]
+
+                    frm = '%Y-%m-%d %H:%M:%S'
+                    self.dt_start_sec = \
+                        int(datetime.strptime(self.dt_start, frm).replace(tzinfo=timezone.utc).timestamp())
+
+        except Exception as ex:
+            r_op_today = [1, 'op_today / ' + str(ex)]
+
+        return r_op_today
+#=======================================================================
+#=======================================================================
 def second_window():
 
     layout = [[sg.Text('The second form is small \nHere to show that opening a window using a window works')],
@@ -73,10 +188,30 @@ def test_menus():
     window.close()
 
 
-def main(args):
-    test_menus()
+def main():
+    while True:  # init db_TODAY ---------------------------------------
+        sg.theme('LightGreen')
+        sg.set_options(element_padding=(0, 0))
+        c_dir    = os.path.abspath(os.curdir)
+        db_TODAY = Class_DB(c_dir + '\\DB\\db_today.sqlite')
+        #db_ARCHV = Class_term_TODAY(c_dir + '\\DB\\db_archv.sqlite')
+        lg_FILE  = Class_LOGGER(    c_dir + '\\LOG\\fut_logger.log')
+        lg_FILE.wr_log_info('START')
+        rq = db_TODAY.op(
+                        rd_cfg_SOFT  = True,
+                        )
+        if rq[0] != 0 : #prn_rq('INIT rd_cfg_SOFT TODAY', rq)
+            print('INIT = > ', rq[1])
+        else:
+            print('INIT cfg_term_data_hist TODAY = > ', rq)
+
+        db_TODAY.prn(
+                        p_cfg_SOFT  = True,
+                        )
+        break
+    #test_menus()
     return 0
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+    sys.exit(main())
